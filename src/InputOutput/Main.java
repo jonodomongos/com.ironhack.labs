b53766ee42cce6c119061cb5af492e8febe9631f
Jonatan Rodríguez <jonodomongos@hotmail.com>
2025-07-08 15:59:42 +0200

    Añadido cuarto ejercicio

--- src/InputOutput/Main.java
+++ src/InputOutput/Main.java
@@ -3,8 +3,7 @@ package InputOutput;
 public class Main {
     public static void main(String[] args) {
         /*
-        Instrucciones
-
+        Instrucciones:
         Pedir al usuario nombre, apellido y edad
         Imprimir frase con estos 3 datos, del tipo:
             “Te llamas Pepe Luis Gonzalez y tienes 45 años”
