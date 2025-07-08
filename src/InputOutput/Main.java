ddacafdd40c95cdc3245e3eecb173f0dc3486d40
Jonatan Rodríguez <jonodomongos@hotmail.com>
2025-07-08 15:55:58 +0200

    Añadido tercer ejercicio

---
+++ src/InputOutput/Main.java
@@ -0,0 +1,14 @@
+package InputOutput;
+
+public class Main {
+    public static void main(String[] args) {
+        /*
+        Instrucciones
+
+        Pedir al usuario nombre, apellido y edad
+        Imprimir frase con estos 3 datos, del tipo:
+            “Te llamas Pepe Luis Gonzalez y tienes 45 años”
+         */
+
+    }
+}
