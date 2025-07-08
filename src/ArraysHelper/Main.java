b83ec2d9cb7b29c4aef9158ff544eef8266b5d07
Jonatan Rodríguez <jonodomongos@hotmail.com>
2025-07-08 16:16:34 +0200

    Añadido séptimo ejercicio

--- src/ArraysHelper/Main.java
+++ src/ArraysHelper/Main.java
@@ -5,8 +5,10 @@ public class Main {
         /*
         Instrucciones:
         1. Crear un array de lo que desees, pero con el tamaño que diga el usuario. Luego añadir elementos hasta que se rellene. Luego mostrarlo.
+
         2. Tienes un array de 20 números enteros: decir cuales son pares e impares.
         +EXTRA: irlos añadiendo a nuevos arrays: 'arrayPares' y 'arrayImpares', y al final mostrar los tres arrays. En este caso, deberias saber por adelantado el length de cada uno (que hay 3 pares y 5 impares), o bien hacer arrays más grandes y dejar los últimos en null.
+
         3. Premio vacaciones en Ibiza: tienes un listado de nombres de personas en un array. Aleatoriamente, seleccionar el ganador y mostrarlo. Entonces preguntar: "otra vez? (S/N)", y si es que sí, volver a generar nuevo ganador y mostrarlo. Seguir preguntando hasta que el usuario diga que no.
          */
     }
---
+++ src/RandomChar/Main.java
@@ -0,0 +1,20 @@
+package RandomChar;
+
+public class Main {
+    public static void main(String[] args) {
+        /*
+        Instrucciones:
+
+        1- El usuario tiene que acertar un número del 1 al 5, generado aleatoriamente. Le vas preguntando hasta que acierte. Al final, decir en cuantos intentos lo ha conseguido.
+
+        2. Tienes un color preseleccionado, por ejemplo el rojo, y le preguntas al usuario un color a ver si lo acierta (solo le preguntas una vez)
+
+        3. Se introduce una frase y se devuelve el texto a la inversa. Ejemplo: olpmejE
+
+        4. Pides un número de teléfono y le quitas, si hay, los espacios, los guiones y los puntos: 456-453-78 quedaría como 45645378
+
+        5. Se pide un texto. Decir si está compuesto solo de minúsculas, solo de mayúsculas o si combina mayúsculas y minúsculas.
+         */
+
+    }
+}
