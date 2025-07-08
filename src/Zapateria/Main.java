e68e3670a42259a0f1a1de205619e11b31aab9b0
Jonatan Rodríguez <jonodomongos@hotmail.com>
2025-07-08 16:19:06 +0200

    Añadido octavo ejercicio

---
+++ src/Zapateria/Main.java
@@ -0,0 +1,27 @@
+package Zapateria;
+
+public class Main {
+    public static void main(String[] args) {
+        /*
+        Instrucciones:
+
+        EJERCICIO TIENDA ZAPATERIA (primera parte)
+        Tienes una zapatería con tres categorías de zapatos: Elegante, Casual, Deporte (una clase cada uno).
+
+        Todos tienen las mismas características: marca, modelo, material, talla y precio. De momento, no hay propiedades
+        distintas.
+
+        En el programa principal, crear al menos dos instancias de cada categoría (zapatos en concreto).
+        Se listan todos los zapatos con sus características.
+        Primero se pregunta al usuario qué tipo de zapato quiere comprar, del tipo:
+        "Que tipo de zapatos quieres comprar?\n1-ELEGANTES\n2-CASUAL\n3-DEPORTIVOS"
+
+        Luego se pregunta al usuario qué modelo quiere comprar. Lo que vale ese modelo, se va sumando al
+        gastoTotal, y se sigue preguntando qué modelo quiere comprar hasta que escribe ‘salir’.
+
+        Cuando sale, se le da el gasto total y un mensaje de ‘hasta la próxima’. El gasto total también se
+        puede ir mostrando dentro del bucle mientras se va sumando.
+         */
+
+    }
+}
